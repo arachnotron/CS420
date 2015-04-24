1b30f9b39a1771955640ea40cae13ca9722eaec7
Jake Siegel <jxs1073@rit.edu>
2015-04-24 17:53:11 -0400

    Built JSON lib and included as reference library instead of referenced project. Working on building out the API accessor class so we don't have to have HTTPGET code all over the place.

--- src/RiotAPI/ApiAccessor.java
+++ src/RiotAPI/ApiAccessor.java
@@ -1,7 +1,10 @@
 package RiotAPI;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.*;
+import org.json.*;
 
 /**
  * Final, singleton class to directly access Riot's RESTful API
@@ -19,7 +22,10 @@ public final class ApiAccessor {
 	 * @throws MalformedURLException
 	 * @throws IOException
 	 */
-	private static void sendGet(String url, String options) throws MalformedURLException, IOException {
+	private static JSONObject sendGet(String url, String options) throws MalformedURLException, IOException {
+		// Build URL. If options != empty string, add & separator
+		if (!options.isEmpty())
+			options += "&";
 		String newUrl = BASE_URL.replace("{url}", url).replace("{options}", options).replace("{apikey}", API_KEY);
 		
 		// Instantiate & open the connection to the API
@@ -31,18 +37,57 @@ public final class ApiAccessor {
 		conn.setRequestProperty("USER_AGENT", USER_AGENT);
 		
 		// Dictate next action based on response
-		switch (conn.getResponseCode()) {
-			case(HttpURLConnection.HTTP_ACCEPTED):
-				conn.disconnect();
-				return;
-			case(423): // HTTP_LOCKED, too many requests
-				conn.disconnect();
-				return;
+		if (conn.getResponseCode() != HttpURLConnection.HTTP_OK){
+			System.err.println("Failed [Code " + conn.getResponseCode() + "] on " + newUrl);
+			conn.disconnect();
+			return null;
 		}
+		
+		BufferedReader br = new BufferedReader( new InputStreamReader(conn.getInputStream()) );
+		StringBuffer sbuff = new StringBuffer();
+		String input;
+		
+		while( (input = br.readLine()) != null ) {
+			sbuff.append(input);
+		}
+		
+		JSONObject response = new JSONObject(sbuff.toString());
+		
+		conn.disconnect();
+		return response;
 	}
 	
-	public static void retrieveSummonerID(String Summoner) throws MalformedURLException, IOException {
-		sendGet("", "");
+	/**
+	 * Retrieves Summoner ID's when given a name. Useful for prediction via user input (web app?)
+	 * @param Summoners Array of Strings of summoner names
+	 * @throws MalformedURLException
+	 * @throws IOException
+	 */
+	public static void retrieveSummonerIDs(String[] Summoners) throws MalformedURLException, IOException {
+		String baseUrl = "v1.4/summoner/by-name/{summonerNames}";
+		StringBuffer summoner = new StringBuffer();
+		
+		for (String name : Summoners) {
+			summoner.append(name);
+			summoner.append(",");
+		}
+		
+		// Just clear the last comma :)
+		summoner.deleteCharAt(summoner.lastIndexOf(","));
+		
+		JSONObject summonerData;
+		try {
+			summonerData = sendGet(baseUrl.replace("{summonerNames}", summoner), "");
+		} catch (NullPointerException e) {
+			// Didn't work. Just return.
+			return;
+		}
+		
+		// for now we're just gonna print stuff
+		for (String key : summonerData.keySet()) {
+			JSONObject summonerObj = summonerData.getJSONObject(key);
+			System.out.println(summonerObj.get("name") + ": " + summonerObj.get("id"));
+		}
 	}
 	
 	public static void retrieveWinRate(long SummonerID) {
@@ -59,5 +104,19 @@ public final class ApiAccessor {
 	
 	public static void retrieveLeague(long SummonerID) {
 		
+	}
+	
+	// test
+	public static void main(String[] args) {
+		try {
+			String[] summoners = new String[2];
+			summoners[0] = "jbiebin";
+			summoners[1] = "solidzer0";
+			
+			retrieveSummonerIDs(summoners);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
---
+++ src/RiotAPI/JsonParser.java
@@ -0,0 +1,5 @@
+package RiotAPI;
+
+public class JsonParser {
+
+}
